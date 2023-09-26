5ed57fd4095a855f79a3cdf24414102343f715b1
Pablo <paablont@gmail.com>
2023-09-26 19:43:21 +0200

    Subida proyecto

---
+++ src/main/java/com/pablito/accesodatos/AccesoDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pablito.accesodatos;
+
+/**
+ *
+ * @author @Pablo Villaseñor Ruiz
+ */
+public class AccesoDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
