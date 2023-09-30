fbc4abab303bc8e252b2fa8b908435d21f5c910d
Pablo <paablont@gmail.com>
2023-09-30 11:22:12 +0200

    Cambios de nombre, JFrames de lectura de TXT

---
+++ src/main/java/com/planeta/PlanetaApp.java
@@ -0,0 +1,15 @@
+package com.planeta;
+
+import lecturaArchivos.LeerTXT;
+
+/**
+ * 
+ * @author @Pablo Villaseñor Ruiz
+ */
+public class PlanetaApp {
+
+    public static void main(String[] args) {
+        LeerTXT ventana=new LeerTXT(); //Declaramos un objeto de la clase Jfram LeerTXT
+        ventana.setVisible(true);
+    }
+}
