50d79f4a5902152f186b866608725c7f4f2bac65
Alumno <miguel5121996@gmail.com>
2023-10-10 21:05:24 +0200

    Creacion y ajuste de metodos escribir y leer en binario Ramon y Miguel

--- src/main/java/com/planeta/RamonMiguelBinario.java
+++ src/main/java/com/planeta/RamonMiguelBinario.java
@@ -1,5 +1,13 @@
-
 package com.planeta;
+
+import Jframes.JFrameCreadorPlaneta;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  *
@@ -7,4 +15,40 @@ package com.planeta;
  */
 public class RamonMiguelBinario {
     
+    public static void ingresarPlaneta(ArrayList<Planeta> p) {
+        for (int i = 0; i < p.size(); i++) {
+            ingresarPlaneta(p.get(i));
+        }
+    }
+
+    public static void ingresarPlaneta(Planeta p) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PlanetaApp.archivoBinario, true))) {
+            oos.writeObject(p);
+            System.out.println("Archivo binario creado con éxito.");
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void leerPlanetaCarac() throws FileNotFoundException, IOException {
+        Planeta plane;
+        ObjectInputStream oIS = new ObjectInputStream(new FileInputStream(PlanetaApp.archivoBinario));
+
+        int i = 1;
+        boolean eof = false;
+        while (!eof) {
+            try {
+                plane = (Planeta) oIS.readObject(); // leer UNA Persona
+                System.out.println(i++ + "=>" + plane);
+            } catch (IOException iOException) {
+                eof = true;
+            } catch (ClassNotFoundException ex) {
+                eof = true;
+                System.out.println("El objeto leÃ­do no era del tipo esperado");
+            }
+        }
+        oIS.close();
+    }
 }
