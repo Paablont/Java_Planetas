00fe65da9a550de95457bfbc9e48518e99554934
Mikel <miguel5121996@gmail.com>
2023-10-10 12:41:31 +0200

    Realizacion clase Satelite

--- src/main/java/com/planeta/Satelite.java
+++ src/main/java/com/planeta/Satelite.java
@@ -5,5 +5,38 @@ package com.planeta;
  * @author @Pablo Villaseñor Ruiz
  */
 public class Satelite {
+    String nombre;
+    double densidad;
+    int anioDescubrimiento;
+
+    public Satelite(String nombre, double densidad, int anioDescubrimiento) {
+        this.nombre = nombre;
+        this.densidad = densidad;
+        this.anioDescubrimiento = anioDescubrimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getDensidad() {
+        return densidad;
+    }
+
+    public void setDensidad(double densidad) {
+        this.densidad = densidad;
+    }
+
+    public int getAnioDescubrimiento() {
+        return anioDescubrimiento;
+    }
+
+    public void setAnioDescubrimiento(int anioDescubrimiento) {
+        this.anioDescubrimiento = anioDescubrimiento;
+    }
     
 }
