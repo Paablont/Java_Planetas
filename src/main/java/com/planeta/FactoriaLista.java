19ac40045f990d3b3d1a71aa98a363913488de30
Pablo <paablont@gmail.com>
2023-10-12 13:05:51 +0200

    FactoriaLista cambiado

--- src/main/java/com/planeta/FactoriaLista.java
+++ src/main/java/com/planeta/FactoriaLista.java
@@ -10,17 +10,35 @@ import java.util.ArrayList;
 public class FactoriaLista {
 
     public static ArrayList<Planeta> creacion() {
-        ArrayList<Satelite> sat = null;
+        ArrayList<Satelite> satelitesMercurio = new ArrayList<>();
+        ArrayList<Satelite> satelitesVenus = new ArrayList<>();
+        ArrayList<Satelite> satelitesTierra = new ArrayList<>();
+        ArrayList<Satelite> satelitesMarte = new ArrayList<>();
+        ArrayList<Satelite> satelitesJupiter = new ArrayList<>();
+        ArrayList<Satelite> satelitesSaturno = new ArrayList<>();
+        ArrayList<Satelite> satelitesUrano = new ArrayList<>();
+        ArrayList<Satelite> satelitesNeptuno = new ArrayList<>();
+        
         ArrayList<Planeta> planetas = new ArrayList<>();
         if (!PlanetaApp.archivo.exists()) {
-            planetas.add(new Planeta(1, "Mercurio", 0.39, 2439.7, false, TipoPlaneta.ROCOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Venus", 0.72, 6051.8, false, TipoPlaneta.ROCOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Tierra", 1.0, 6371.0, true, TipoPlaneta.ROCOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Marte", 1.52, 3389.5, false, TipoPlaneta.ROCOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Júpiter", 5.2, 69911.0, false, TipoPlaneta.GASEOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Saturno", 9.58, 58232.0, false, TipoPlaneta.GASEOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Urano", 19.22, 25362.0, false, TipoPlaneta.GASEOSO, sat));
-            planetas.add(new Planeta(planetas.size() + 1, "Neptuno", 30.05, 24622.0, false, TipoPlaneta.GASEOSO, sat));
+            // Satélites reales de los planetas
+            satelitesMercurio.add(new Satelite("Sin satélite", 0.0, "N/A")); // Mercurio
+            satelitesVenus.add(new Satelite("Sin satélite", 0.0, "N/A")); // Venus
+            satelitesTierra.add(new Satelite("Luna", 3.34, "20/07/1969")); // Tierra
+            satelitesMarte.add(new Satelite("Fobos", 1.88, "18/08/1877")); // Marte
+            satelitesJupiter.add(new Satelite("Ganímedes", 1.94, "11/01/1610")); // Júpiter
+            satelitesSaturno.add(new Satelite("Titán", 1.88, "25/03/1655")); // Saturno
+            satelitesUrano.add(new Satelite("Miranda", 1.20, "16/02/1948")); // Urano
+            satelitesNeptuno.add(new Satelite("Tritón", 2.05, "10/10/1846")); // Neptuno
+
+            planetas.add(new Planeta(1, "Mercurio", 0.39, 2439.7, false, TipoPlaneta.ROCOSO, satelitesMercurio));
+            planetas.add(new Planeta(planetas.size() + 1, "Venus", 0.72, 6051.8, false, TipoPlaneta.ROCOSO, satelitesVenus));
+            planetas.add(new Planeta(planetas.size() + 1, "Tierra", 1.0, 6371.0, true, TipoPlaneta.ROCOSO, satelitesTierra));
+            planetas.add(new Planeta(planetas.size() + 1, "Marte", 1.52, 3389.5, false, TipoPlaneta.ROCOSO, satelitesMarte));
+            planetas.add(new Planeta(planetas.size() + 1, "Júpiter", 5.2, 69911.0, false, TipoPlaneta.GASEOSO, satelitesJupiter));
+            planetas.add(new Planeta(planetas.size() + 1, "Saturno", 9.58, 58232.0, false, TipoPlaneta.GASEOSO, satelitesSaturno));
+            planetas.add(new Planeta(planetas.size() + 1, "Urano", 19.22, 25362.0, false, TipoPlaneta.GASEOSO, satelitesUrano));
+            planetas.add(new Planeta(planetas.size() + 1, "Neptuno", 30.05, 24622.0, false, TipoPlaneta.GASEOSO, satelitesNeptuno));
         }
         return planetas;
     }
