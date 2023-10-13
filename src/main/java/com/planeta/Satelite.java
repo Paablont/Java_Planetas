b88f9a50db201237f00cb57a0f42622607525a57
Pablo <paablont@gmail.com>
2023-10-13 10:08:13 +0200

    Satelite cambio el String nombre por char[] nombre y calculado el tamaño del registro

--- src/main/java/com/planeta/Satelite.java
+++ src/main/java/com/planeta/Satelite.java
@@ -2,6 +2,7 @@ package com.planeta;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  *
@@ -11,25 +12,44 @@ public class Satelite implements Serializable {
    
     public static final String archivoRAF = "." + File.separatorChar + "archivos" + File.separatorChar + "satelites" + File.separatorChar;
     public static final int TAMAÑO_REGISTRO = 88; //30bytes al String *2bytes + 8 bytes de double + 10bytes a fechaDescubrimiento * 2 bytes) = 
+    public static final int TAMAÑO_NOMBRE = 30;
     
-    String nombre;
+    private char[] nombre = new char[Satelite.TAMAÑO_NOMBRE];
     double densidad;
     String fechaDescubrimiento;
     
 
     
-    public Satelite(String nombre, double densidad, String fechaDescubrimiento) {
+    public Satelite(char[] nombre, double densidad, String fechaDescubrimiento) {
         this.nombre = nombre;
         this.densidad = densidad;
         this.fechaDescubrimiento = fechaDescubrimiento;
     }
 
-    public String getNombre() {
-        return nombre;
+    public char[] getNombreCharArray() {
+        return this.nombre;
+    }
+
+    public byte[] getNombreByteArray() {
+        byte[] nombre_bytes = new byte[this.nombre.length * 2];
+
+        for (int i = 0; i < this.nombre.length; i++) {
+            nombre_bytes[i * 2] = (byte) (this.nombre[i] >> 8);
+            nombre_bytes[i * 2 + 1] = (byte) this.nombre[i];
+        }
+        return nombre_bytes;
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.setNombre(nombre.toCharArray());
+    }
+
+    public void setNombre(char[] nombre) {
+        //sobrescribo el array completo, no lo creo de nuevo. ahorro memoria
+        Arrays.fill(this.nombre, (char) 0);
+        for (int i = 0; i < nombre.length && i < this.nombre.length; i++) {
+            this.nombre[i] = nombre[i];
+        }
     }
 
     public double getDensidad() {
