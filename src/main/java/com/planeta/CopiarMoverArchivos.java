8de1bcdf74753d7535abf416ca595bedfdf49923
Pablo <paablont@gmail.com>
2023-10-17 12:03:13 +0200

    metodos preparados

--- src/main/java/com/planeta/CopiarMoverArchivos.java
+++ src/main/java/com/planeta/CopiarMoverArchivos.java
@@ -6,4 +6,11 @@ package com.planeta;
  */
 public class CopiarMoverArchivos {
     
+    public static void copiarArchivo(String rutaArchivoOriginal,String rutanuevoArchivo){
+        
+    }
+    
+    public static void moverArchivo(String ruta){
+        
+    }
 }
