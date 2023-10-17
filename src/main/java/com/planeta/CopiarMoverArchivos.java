46cb133347c0f33c0ad305bdd8c1e3d7c306dbb7
Pablo <paablont@gmail.com>
2023-10-17 13:09:04 +0200

    CopiarMoverArchivo con metodo de copiar el txt (se me olvido añadirlo en el anterior commit)

--- src/main/java/com/planeta/CopiarMoverArchivos.java
+++ src/main/java/com/planeta/CopiarMoverArchivos.java
@@ -1,16 +1,31 @@
 package com.planeta;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author pablo y Ramon
  */
 public class CopiarMoverArchivos {
     
-    public static void copiarArchivo(String rutaArchivoOriginal,String rutanuevoArchivo){
+    public static void copiarArchivo(){
+        File archivoOrigen = new File(Planeta.archivoTXT);
+        File archivoNuevo = new File("." + File.separatorChar +"copiaArchivoTXT.txt");
         
-    }
-    
-    public static void moverArchivo(String ruta){
-        
+         try {
+            // Copiar el archivo
+            Path origen = archivoOrigen.toPath();
+            Path destino = archivoNuevo.toPath();
+            Files.copy(origen, destino, StandardCopyOption.REPLACE_EXISTING);
+
+             JOptionPane.showMessageDialog(null, "Archivo copiado correctamente en el mismo directorio del proyecto", "Mensaje ID", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException e) {
+            PlanetaApp.logger.error("No se ha podido copiar el fichero txt");
+        }
     }
 }
